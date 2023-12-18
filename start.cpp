ced1c7cfcc377e52559208ec9d9126ccaacf5968
Elvis-321 <eniflaz@unsa.edu.pe>
2023-12-17 20:45:00 -0500

    he añadido el slot on_pushButton_clicked para manejar el evento de hacer clic en el boton inicio

--- start.cpp
+++ start.cpp
@@ -1,14 +1,25 @@
 #include "start.h"
 #include "ui_start.h"
+#include "gamewindow.h"
 
+// Constructor de la clase Start: inicializa la interfaz de usuario
 Start::Start(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::Start)
-{
-    ui->setupUi(this);
+    ui(new Ui::Start) {
+    ui->setupUi(this);  // Configura la interfaz de usuario según la definición en el archivo .ui
 }
 
-Start::~Start()
-{
-    delete ui;
+// Destructor de Start: limpia los recursos de la interfaz de usuario
+Start::~Start() {
+    delete ui;  // Libera la memoria de la interfaz de usuario
+}
+
+// Manejador para el evento de clic en el botón de inicio
+void Start::on_pushButton_clicked() {
+    // Crea y muestra una instancia de GameWindow
+    GameWindow *gameWindow = new GameWindow();
+    gameWindow->show();
+
+    // Cierra la ventana actual (Start)
+    this->close();
 }
--- start.h
+++ start.h
@@ -4,19 +4,21 @@
 #include <QWidget>
 
 namespace Ui {
-class Start;
+class Start;  // Clase de interfaz de usuario para la ventana de inicio
 }
 
-class Start : public QWidget
-{
+class Start : public QWidget {
     Q_OBJECT
 
 public:
-    explicit Start(QWidget *parent = nullptr);
-    ~Start();
+    explicit Start(QWidget *parent = nullptr);  // Constructor de la ventana de inicio
+    ~Start();  // Destructor
+
+private slots:
+    void on_pushButton_clicked();  // Manejador del clic en el botón de inicio
 
 private:
-    Ui::Start *ui;
+    Ui::Start *ui;  // Interfaz de usuario
 };
 
 #endif // START_H
