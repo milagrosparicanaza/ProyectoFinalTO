04f3ccef89f60ab02345539935a40acd2d56f6ce
Elvis-321 <eniflaz@unsa.edu.pe>
2023-12-17 20:50:15 -0500

    he implementado el constructor de Trash y su funcion updatePosition

--- trash.cpp
+++ trash.cpp
@@ -1,6 +1,22 @@
 #include "trash.h"
+#include <QRandomGenerator>
+#include <QWidget>
 
-Trash::Trash()
-{
+// Constructor de Trash: inicializa un objeto Trash con posición y características aleatorias
+Trash::Trash(QObject *parent)
+    : GameObject(QRandomGenerator::global()->bounded(static_cast<QWidget*>(parent)->width()),
+                 -50, // Inicia justo fuera de la vista en la parte superior
+                 50, GameObject::Square, Qt::red, parent) {
+    speed = 2;  // Establece velocidad de caída
+}
 
+// Actualiza la posición de Trash en cada frame
+void Trash::updatePosition() {
+    int dy = speed;  // Calcula el desplazamiento vertical
+    rect.translate(0, dy);  // Mueve el objeto hacia abajo
+
+    // Verifica si la basura ha salido de la pantalla
+    if (rect.top() > parentWidget->height()) {
+        emit trashOutOfScreen(this); // Emite señal si sale de la pantalla
+    }
 }
--- trash.h
+++ trash.h
@@ -1,11 +1,17 @@
 #ifndef TRASH_H
 #define TRASH_H
 
+#include "gameobject.h"
 
-class Trash
-{
+class Trash : public GameObject {
+    Q_OBJECT
+
 public:
-    Trash();
+    explicit Trash(QObject *parent = nullptr);  // Constructor para el objeto Trash
+    void updatePosition() override;  // Actualiza posición, sobrescribe método de GameObject
+
+signals:
+    void trashOutOfScreen(Trash *trash);  // Señal emitida cuando la basura sale de pantalla
 };
 
 #endif // TRASH_H
